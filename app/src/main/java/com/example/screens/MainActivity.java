0cdd3c8029c9d472c98abe624644ce69b1d2b20f
a19chrel <a19chrel@student.his.se>
2020-04-23 12:25:37 +0200

    Added: Fragment

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                intent.putExtra("name", "daniel");
                 startActivity(intent);
             }
         });
